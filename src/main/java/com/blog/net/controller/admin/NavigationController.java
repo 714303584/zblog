18dccf9f3de368c492eada802e9e18e8c033c0e2
714303584@qq.com <714303584@qq.com>
2016-06-06 18:38:26 +0800

    Navigation 分页以及重定向！

--- src/main/java/com/blog/net/controller/admin/NavigationController.java
+++ src/main/java/com/blog/net/controller/admin/NavigationController.java
@@ -1,7 +1,12 @@
 package com.blog.net.controller.admin;
 
+import java.io.File;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.enterprise.inject.Default;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,10 +14,12 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.blog.dao.NavigationDaoImpl;
 import com.blog.entity.Navigation;
 import com.blog.entity.User;
+import com.blog.util.page.DefaultPage;
 
 @Controller("adminNavigationController ")
 @RequestMapping("/admin/navigation")
@@ -26,18 +33,14 @@ public class NavigationController {
 		navigation.setcreateDate(new Date());
 		navigation.setmodifyDate(new Date());
 		navigationDaoImpl.save(navigation);
-		List<Navigation> navigations = navigationDaoImpl.findListBy(null);
-		model.put("navigations", navigations);
-		return "navigation/list";
+		return "redirect:/admin/navigation/list.html";
 	}
 
 	@RequestMapping(value = "/edit", method = { RequestMethod.POST })
 	public String edit(Navigation navigation, ModelMap model) {
 		navigation.setmodifyDate(new Date());
 		navigationDaoImpl.update(navigation);
-		List<Navigation> navigations = navigationDaoImpl.findListBy(null);
-		model.put("navigations", navigations);
-		return "navigation/list";
+		return "redirect:/admin/navigation/list.html";
 	}
 
 	@RequestMapping(value = "/deleteByIds", method = { RequestMethod.POST })
@@ -45,22 +48,40 @@ public class NavigationController {
 		if (ids != null) {
 			navigationDaoImpl.deleteByIds(ids);
 		}
-		List<Navigation> navigations = navigationDaoImpl.findListBy(null);
-		model.put("navigations", navigations);
-		return "navigation/list";
+		return "redirect:/admin/navigation/list.html";
 	}
 
 	@RequestMapping(value = "/list", method = { RequestMethod.GET })
-	public String list(Navigation navigation, ModelMap model) {
-		List<Navigation> navigations = navigationDaoImpl.findListBy(null);
-		model.put("navigations", navigations);
+	public String list(@RequestParam Map<String, Object> filter, ModelMap model) {
+		DefaultPage<Navigation> pages = new DefaultPage<Navigation>();
+		
+		Object pageVal =  filter.get("page");
+		if(pageVal != null){
+		 int page = Integer.parseInt(pageVal.toString()) - 1;
+			filter.remove("page");
+			pages.setPageNo(page);
+		}
+		
+		Object pageSizeVal = filter.get("pageSize");
+		if(pageSizeVal != null){
+		  int pageSize = Integer.parseInt(pageSizeVal.toString());
+			filter.remove("pageSize");
+			pages.setPageSize(pageSize);
+		}
+		
+		int total = navigationDaoImpl.getCountBy(filter);
+		
+		pages.setTotalCount(total);
+		
+		filter.put("page", pages.pageSql());
+		List<Navigation> navigations = navigationDaoImpl.findPageBy(filter);
+		pages.setElements(navigations);
+		model.put("pages", pages);
 		return "navigation/list";
 	}
 
 	@RequestMapping(value = "/add", method = { RequestMethod.GET })
 	public String add(ModelMap model) {
-		List<Navigation> navigations = navigationDaoImpl.findListBy(null);
-		model.put("navigations", navigations);
 		return "navigation/add";
 	}
 
@@ -78,9 +99,7 @@ public class NavigationController {
 		long[] ids = new long[1];
 		ids[0] = id;
 		navigationDaoImpl.deleteByIds(ids);
-		List<Navigation> navigations = navigationDaoImpl.findListBy(null);
-		model.put("navigations", navigations);
-		return "navigation/list";
+		return "redirect:/admin/navigation/list.html";
 	}
 
 }
