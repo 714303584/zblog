6949e265d11234f30292320b76902aeac420c31c
714303584@qq.com <714303584@qq.com>
2016-08-19 15:27:25 +0800

    DataStatus

---
+++ src/main/java/com/blog/util/DataStatus.java
@@ -0,0 +1,19 @@
+package com.blog.util;
+
+public class DataStatus {
+	
+	
+	// created and  unaudited
+	public final static int CREATED = 1;
+	
+	//audited and passed
+	public final static int PASSED = 2;
+	
+	//audited and unpassed
+	public final static int UNPASSED = 3;
+		
+	//deled
+	public final static int DEL = 4;
+	
+
+}
