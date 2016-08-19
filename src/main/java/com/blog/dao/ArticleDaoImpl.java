582af9e650fd86cab6fe49769638f2cece653d31
714303584@qq.com <714303584@qq.com>
2016-08-19 15:27:57 +0800

    ArticleDaoImpl

--- src/main/java/com/blog/dao/ArticleDaoImpl.java
+++ src/main/java/com/blog/dao/ArticleDaoImpl.java
@@ -1,6 +1,8 @@
 package com.blog.dao;
 
 import com.blog.entity.Article;
+import com.blog.entity.Navigation;
+
 import java.util.List;
 import java.util.Map;
 
@@ -16,6 +18,14 @@ public interface ArticleDaoImpl {
 	public void deleteByIds(long[] ids);
 	
 	public void update(Article entity);
+	
+	
+	public List<Navigation> findPageBy(Map<String, Object> map);
+	
+	
+	public void update(Navigation entity);
+	
+	public int getCountBy(Map<String, Object> map);
 
 }
 	
